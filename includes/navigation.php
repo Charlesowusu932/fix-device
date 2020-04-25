a735c0bcc43040dcdfa7c8cf689f7408625d10af
Charles Owusu <kofidesign932@gmail.com>
2020-04-25 18:28:04 +0000

    Update

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,61 +1,52 @@
-
-
-
 <nav class="fixed-top  showit">
     <div class="container">
         <div class="row py-3">
-        <div class="col-4 col-md-4">
-        <a href="index.php"><img src="img/fixdevicelogo.svg" class="img-fluid" width="150"></a>
+            <div class="col-4 col-md-4">
+                <a href="index.php"><img src="img/fixdevicelogo.svg" class="img-fluid" width="150"></a>
+            </div>
+            <div class="col-8 col-md-8">
+                <!-- small details at the top -->
+                <ul class="nav justify-content-end">
+                    <li class="nav-item">
+                        <a class="nav-link smallnavtop" href="login.php">Login as a member</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link smallnavtop" href="signup.php">Register </a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link smallnavtop" href="#">+233 234 7892 34</a>
+                    </li>
+                </ul>
+                <!-- small details at the top -->
+
+                <!-- main nav detials -->
+                <ul class="nav justify-content-end">
+                    <li class="nav-item  ">
+                        <a class="nav-link pl-4 mainav active" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link pl-4 mainav" href="about_us.php">About Us</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link pl-4 mainav" href="services.php">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link pl-4 mainav" href="technicians.php">Technicians</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link pl-4 mainav" href="contact.php">Contact Us</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link pl-4 mainav btn btn-primary" href="fix.php">Fix a device</a>
+                    </li>
+                </ul>
+                <!-- main nav detials -->
+
+            </div>
         </div>
-        <div class="col-8 col-md-8">
-        <!-- small details at the top -->
-        <ul class="nav justify-content-end">
-            <li class="nav-item">
-               <a class="nav-link smallnavtop" href="login.php">Login as a member</a>
-            </li>
-            <li class="nav-item">
-               <a class="nav-link smallnavtop" href="signup.php">Register </a>
-            </li>
-        
-            <li class="nav-item">
-               <a class="nav-link smallnavtop" href="#">+233 234 7892 34</a>
-            </li>
-          </ul>
-          <!-- small details at the top -->
-
-
-          <!-- main nav detials -->
-        <ul class="nav justify-content-end">
-            <li class="nav-item  ">
-            <a class="nav-link pl-4 mainav active" href="index.php">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link pl-4 mainav" href="about_us.php">About Us</a>
-          </li>
-        
-          <li class="nav-item">
-            <a class="nav-link pl-4 mainav" href="services.php">Services</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link pl-4 mainav" href="technicians.php">Technicians</a>
-          </li>
-          
-          <li class="nav-item">
-            <a class="nav-link pl-4 mainav" href="contact.php">Contact Us</a>
-          </li>
-
-          <li class="nav-item">
-            <a class="nav-link pl-4 mainav btn btn-primary" href="fix.php">Fix a device</a>
-          </li>
-          </ul>
-          <!-- main nav detials -->
-
-
-
-        </div>
-      </div>
     </div>
 </nav>
-
-
-
--- login.php
+++ login.php
@@ -2,66 +2,66 @@
 <html lang="en">
 
 <head>
-<title>Fix Devices | Login</title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<meta name="description" content="">
-<meta name="keywords" content="">
-<meta name="author" content="">
+    <title>Fix Devices | Login</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
 
-<!-- include navigation -->
-<?php require_once("includes/headerlinks.php");?>
+    <!-- include navigation -->
+    <?php require_once("includes/headerlinks.php");?>
 
-<!-- include navigation -->
+        <!-- include navigation -->
 
-<div class="sub1a">
-<div class="left-covera"></div>
-<div class="container-fluid ">
-<div class="row">
-<div class="col-12 col-md-5">
-</div>
-<div class="col-12 col-md-7 ">
+        <div class="sub1a">
+            <div class="left-covera"></div>
+            <div class="container-fluid ">
+                <div class="row">
+                    <div class="col-12 col-md-5">
+                    </div>
+                    <div class="col-12 col-md-7 ">
 
-<div class="row">
-<div class="col-12 col-md-8 col-lg-7 mx-auto">
-<div class="text-center mb-5">
-<a href="index.php"><img src="img/fixdevicelogo1.svg" class="img-fluid" width="150"></a>
+                        <div class="row">
+                            <div class="col-12 col-md-8 col-lg-7 mx-auto">
+                                <div class="text-center mb-5">
+                                    <a href="index.php"><img src="img/fixdevicelogo1.svg" class="img-fluid" width="150"></a>
 
-<h1 class="mt-4"><strong>Please sign in.</strong> </h1>
-<p>Welcome to Fix Device</p>
-</div>
+                                    <h1 class="mt-4"><strong>Please sign in.</strong> </h1>
+                                    <p>Welcome to Fix Device</p>
+                                </div>
 
-<div>
-<form>
-<div class="form-group">
-<input type="email" class="form-control form-fix" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Email / Student ID">
-</div>
-<div class="form-group">
-<input type="password" class="form-control form-fix" id="exampleInputPassword1" placeholder="Password">
-</div>
-<small class="mb-4"><span> <a href="signup.php">Forgot password?
-</a> </span></small>
-<div class="mt-4">
-<button type="submit" class="btn btn-primary btn-lg btn-block ">Log in</button>
-</div>
+                                <div>
+                                    <form>
+                                        <div class="form-group">
+                                            <input type="email" class="form-control form-fix" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Email / Student ID">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" class="form-control form-fix" id="exampleInputPassword1" placeholder="Password">
+                                        </div>
+                                        <small class="mb-4"><span> <a href="signup.php">Forgot password?
+                                        </a> </span></small>
+                                        <div class="mt-4">
+                                            <button type="submit" class="btn btn-primary btn-lg btn-block ">Log in</button>
+                                        </div>
 
-<div class="text-center mt-5">
-<p>Dont have an account ? <span> <a href="signup.php">Sign up </a> </span></p>
-</div>
+                                        <div class="text-center mt-5">
+                                            <p>Dont have an account ? <span> <a href="signup.php">Sign up </a> </span></p>
+                                        </div>
 
-</form>
-</div>
-</div>
-</div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
 
-</div>
-</div>
+                    </div>
+                </div>
 
-</div>
-</div>
+            </div>
+        </div>
 
-<!-- top cover -->
-<!-- <section class="topcover centerit">
+        <!-- top cover -->
+        <!-- <section class="topcover centerit">
 <div class="container "> 
 <div class="row ">
 
@@ -86,12 +86,12 @@ Our Plartfrom connects electrical device repairs to students who need thier devi
 </div>
 </div>
 </section> -->
-<!-- top cover -->
+        <!-- top cover -->
 
-<!-- footer includes -->
-<?php require_once("includes/footerlinks.php");?>
-<!-- footer includes -->
+        <!-- footer includes -->
+        <?php require_once("includes/footerlinks.php");?>
+            <!-- footer includes -->
 
-</body>
+            </body>
 
 </html>
