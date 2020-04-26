20475d9fa8b67802d0ef3a1c8cfa5ec0766b7849
Charles Owusu <kofidesign932@gmail.com>
2020-04-26 01:46:59 +0000

    Add Contact PAge

---
+++ contact.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Fix Devices | Contact Us</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+
+    <!-- include navigation -->
+    <?php require_once("includes/headerlinks.php");?>
+    <?php require_once("includes/navigation.php");?>
+            <!-- include navigation -->
+
+            <!-- top cover -->
+            <section class="contact centerit">
+                <div class="container ">
+                    <div class="row ">
+
+                        <div class="col-12 text-center mt-5 ">
+                            <h1 class="bighead">Get in Touch</h1>
+                           
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- top cover -->
+
+
+            <!-- contact info and map -->
+              <section class="fixglobalspace2">
+               <div class="container">
+                   <div class="row">
+                       <div class="col-12 col-md-8">
+                           <h1> <strong>Contact Us</strong> </h1>
+                           <P class="biggerf">Do not hesitate to reach out. Just fill in the contact form here and <br>  we’ll be sure to reply as fast as possible.</P>
+
+                           <div class="row mt-5">
+                               <div class="col-12 col-md-6">
+                                   <h5><strong>Public relations</strong></h5>
+                                   <p>For press, speaking or marketing related inquiries</p>
+
+                                   <p>
+                                     <img src="img/mail.svg" width="30" alt=""> <span>relations@fixdevice.com</span>
+                                   </p>
+                                   
+                                   <p>
+                                     <img src="img/phonec.svg" width="30" alt=""> <span>+233 24 677 1647</span>
+                                   </p>
+                               </div>
+
+                               <div class="col-12 col-md-6">
+                                   <h5><strong>Business submissions</strong></h5>
+                                   <p>Any Technician looking for help, kindly contact us here</p>
+
+                                   <p>
+                                     <img src="img/mail.svg" width="30" alt=""> <span>business@fixdevice.com</span>
+                                   </p>
+                                   
+                                   <p>
+                                     <img src="img/phonec.svg" width="30" alt=""> <span>+233 24 677 1647</span>
+                                   </p>
+                               </div>
+
+
+                               
+                           </div>
+                       </div>
+                       <div class="col-12 col-md-4"></div>
+                   </div>
+               </div>
+
+              </section>
+             <!-- contact info and map -->
+
+           
+
+      
+          
+
+            <!-- footer includes -->
+            <?php require_once("includes/footernav.php");?>
+                <?php require_once("includes/footerlinks.php");?>
+                    <!-- footer includes -->
+
+                    </body>
+
+</html>
