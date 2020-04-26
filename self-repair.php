cc10f5674c59a3c7f86e7dcd86808eca3231737b
Charles Owusu <kofidesign932@gmail.com>
2020-04-26 11:43:10 +0000

    Update Self Repair

--- self-repair.php
+++ self-repair.php
@@ -11,7 +11,7 @@
 
     <!-- include navigation -->
     <?php require_once("includes/headerlinks.php");?>
-    <?php require_once("includes/navigation.php");?>
+        <?php require_once("includes/navigation.php");?>
             <!-- include navigation -->
 
             <!-- top cover -->
@@ -22,7 +22,6 @@
                         <div class="col-12 text-center mt-5 ">
                             <h1 class="bighead">Self Repair</h1>
                             <p class="white mt-3">Want to get your hands dirty, follow our videos and fix it yourself</p>
-                           
 
                         </div>
                     </div>
@@ -30,182 +29,165 @@
             </section>
             <!-- top cover -->
 
-
-
-
-                  <!-- videos  -->
+            <!-- videos  -->
             <div class="fixglobalspace2">
                 <div class="container">
                     <div class="row">
 
                         <div class="col-12 col-md-6 col-lg-4 mb-4 ">
                             <div class="card h-100">
-                            <div class="embed-responsive embed-responsive-16by9">
-                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/CDZelG2AusY" allowfullscreen></iframe>
-                            </div>
+                                <div class="embed-responsive embed-responsive-16by9">
+                                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/CDZelG2AusY" allowfullscreen></iframe>
+                                </div>
                                 <div class="card-body">
-                                <h6 class="mb-0">  <strong>iOS 13.3 Update Without WiFi</strong></h6>
-                               </div>
-                            </div>
-                        </div>
-
-
-                        <div class="col-12 col-md-6 col-lg-4 mb-4 ">
-                            <div class="card h-100">
-                            <div class="embed-responsive embed-responsive-16by9">
-                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/GUzBWFqTKwM" allowfullscreen></iframe>
-                            </div>
-                                <div class="card-body">
-                                <h6 class="mb-0">  <strong>How to replace a laptop keyboard</strong></h6>
-                               </div>
+                                    <h6 class="mb-0">  <strong>iOS 13.3 Update Without WiFi</strong></h6>
+                                </div>
                             </div>
                         </div>
 
                         <div class="col-12 col-md-6 col-lg-4 mb-4 ">
                             <div class="card h-100">
-                            <div class="embed-responsive embed-responsive-16by9">
-                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/JIdQw_K-ArY" allowfullscreen></iframe>
-                            </div>
+                                <div class="embed-responsive embed-responsive-16by9">
+                                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/GUzBWFqTKwM" allowfullscreen></iframe>
+                                </div>
                                 <div class="card-body">
-                                <h6 class="mb-0">  <strong>How to upgrade the PS4 with a SSD - HyperX</strong></h6>
-                               </div>
+                                    <h6 class="mb-0">  <strong>How to replace a laptop keyboard</strong></h6>
+                                </div>
                             </div>
                         </div>
 
                         <div class="col-12 col-md-6 col-lg-4 mb-4 ">
                             <div class="card h-100">
-                            <div class="embed-responsive embed-responsive-16by9">
-                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/GUzBWFqTKwM" allowfullscreen></iframe>
-                            </div>
+                                <div class="embed-responsive embed-responsive-16by9">
+                                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/JIdQw_K-ArY" allowfullscreen></iframe>
+                                </div>
                                 <div class="card-body">
-                                <h6 class="mb-0">  <strong>How to replace a laptop keyboard</strong></h6>
-                               </div>
+                                    <h6 class="mb-0">  <strong>How to upgrade the PS4 with a SSD - HyperX</strong></h6>
+                                </div>
                             </div>
                         </div>
 
                         <div class="col-12 col-md-6 col-lg-4 mb-4 ">
                             <div class="card h-100">
-                            <div class="embed-responsive embed-responsive-16by9">
-                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/JIdQw_K-ArY" allowfullscreen></iframe>
-                            </div>
+                                <div class="embed-responsive embed-responsive-16by9">
+                                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/GUzBWFqTKwM" allowfullscreen></iframe>
+                                </div>
                                 <div class="card-body">
-                                <h6 class="mb-0">  <strong>How to upgrade the PS4 with a SSD - HyperX</strong></h6>
-                               </div>
+                                    <h6 class="mb-0">  <strong>How to replace a laptop keyboard</strong></h6>
+                                </div>
                             </div>
                         </div>
 
                         <div class="col-12 col-md-6 col-lg-4 mb-4 ">
                             <div class="card h-100">
-                            <div class="embed-responsive embed-responsive-16by9">
-                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/CDZelG2AusY" allowfullscreen></iframe>
-                            </div>
+                                <div class="embed-responsive embed-responsive-16by9">
+                                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/JIdQw_K-ArY" allowfullscreen></iframe>
+                                </div>
                                 <div class="card-body">
-                                <h6 class="mb-0">  <strong>iOS 13.3 Update Without WiFi</strong></h6>
-                               </div>
+                                    <h6 class="mb-0">  <strong>How to upgrade the PS4 with a SSD - HyperX</strong></h6>
+                                </div>
                             </div>
                         </div>
 
                         <div class="col-12 col-md-6 col-lg-4 mb-4 ">
                             <div class="card h-100">
-                            <div class="embed-responsive embed-responsive-16by9">
-                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/CDZelG2AusY" allowfullscreen></iframe>
-                            </div>
+                                <div class="embed-responsive embed-responsive-16by9">
+                                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/CDZelG2AusY" allowfullscreen></iframe>
+                                </div>
                                 <div class="card-body">
-                                <h6 class="mb-0">  <strong>iOS 13.3 Update Without WiFi</strong></h6>
-                               </div>
-                            </div>
-                        </div>
-
-
-                        <div class="col-12 col-md-6 col-lg-4 mb-4 ">
-                            <div class="card h-100">
-                            <div class="embed-responsive embed-responsive-16by9">
-                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/GUzBWFqTKwM" allowfullscreen></iframe>
-                            </div>
-                                <div class="card-body">
-                                <h6 class="mb-0">  <strong>How to replace a laptop keyboard</strong></h6>
-                               </div>
+                                    <h6 class="mb-0">  <strong>iOS 13.3 Update Without WiFi</strong></h6>
+                                </div>
                             </div>
                         </div>
 
                         <div class="col-12 col-md-6 col-lg-4 mb-4 ">
                             <div class="card h-100">
-                            <div class="embed-responsive embed-responsive-16by9">
-                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/JIdQw_K-ArY" allowfullscreen></iframe>
-                            </div>
+                                <div class="embed-responsive embed-responsive-16by9">
+                                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/CDZelG2AusY" allowfullscreen></iframe>
+                                </div>
                                 <div class="card-body">
-                                <h6 class="mb-0">  <strong>How to upgrade the PS4 with a SSD - HyperX</strong></h6>
-                               </div>
+                                    <h6 class="mb-0">  <strong>iOS 13.3 Update Without WiFi</strong></h6>
+                                </div>
                             </div>
                         </div>
 
                         <div class="col-12 col-md-6 col-lg-4 mb-4 ">
                             <div class="card h-100">
-                            <div class="embed-responsive embed-responsive-16by9">
-                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/GUzBWFqTKwM" allowfullscreen></iframe>
-                            </div>
+                                <div class="embed-responsive embed-responsive-16by9">
+                                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/GUzBWFqTKwM" allowfullscreen></iframe>
+                                </div>
                                 <div class="card-body">
-                                <h6 class="mb-0">  <strong>How to replace a laptop keyboard</strong></h6>
-                               </div>
+                                    <h6 class="mb-0">  <strong>How to replace a laptop keyboard</strong></h6>
+                                </div>
                             </div>
                         </div>
 
                         <div class="col-12 col-md-6 col-lg-4 mb-4 ">
                             <div class="card h-100">
-                            <div class="embed-responsive embed-responsive-16by9">
-                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/JIdQw_K-ArY" allowfullscreen></iframe>
-                            </div>
+                                <div class="embed-responsive embed-responsive-16by9">
+                                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/JIdQw_K-ArY" allowfullscreen></iframe>
+                                </div>
                                 <div class="card-body">
-                                <h6 class="mb-0">  <strong>How to upgrade the PS4 with a SSD - HyperX</strong></h6>
-                               </div>
+                                    <h6 class="mb-0">  <strong>How to upgrade the PS4 with a SSD - HyperX</strong></h6>
+                                </div>
                             </div>
                         </div>
 
                         <div class="col-12 col-md-6 col-lg-4 mb-4 ">
                             <div class="card h-100">
-                            <div class="embed-responsive embed-responsive-16by9">
-                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/CDZelG2AusY" allowfullscreen></iframe>
-                            </div>
+                                <div class="embed-responsive embed-responsive-16by9">
+                                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/GUzBWFqTKwM" allowfullscreen></iframe>
+                                </div>
                                 <div class="card-body">
-                                <h6 class="mb-0">  <strong>iOS 13.3 Update Without WiFi</strong></h6>
-                               </div>
+                                    <h6 class="mb-0">  <strong>How to replace a laptop keyboard</strong></h6>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-12 col-md-6 col-lg-4 mb-4 ">
+                            <div class="card h-100">
+                                <div class="embed-responsive embed-responsive-16by9">
+                                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/JIdQw_K-ArY" allowfullscreen></iframe>
+                                </div>
+                                <div class="card-body">
+                                    <h6 class="mb-0">  <strong>How to upgrade the PS4 with a SSD - HyperX</strong></h6>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-12 col-md-6 col-lg-4 mb-4 ">
+                            <div class="card h-100">
+                                <div class="embed-responsive embed-responsive-16by9">
+                                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/CDZelG2AusY" allowfullscreen></iframe>
+                                </div>
+                                <div class="card-body">
+                                    <h6 class="mb-0">  <strong>iOS 13.3 Update Without WiFi</strong></h6>
+                                </div>
                             </div>
                         </div>
 
                         <!-- pagnation -->
                         <div class="col-12 mt-4">
                             <nav aria-label="Page navigation example">
-                            <ul class="pagination justify-content-center">
-                                <li class="page-item disabled">
-                                <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
-                                </li>
-                                <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                <li class="page-item">
-                                <a class="page-link" href="#">Next</a>
-                                </li>
-                            </ul>
+                                <ul class="pagination justify-content-center">
+                                    <li class="page-item disabled">
+                                        <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+                                    </li>
+                                    <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                    <li class="page-item">
+                                        <a class="page-link" href="#">Next</a>
+                                    </li>
+                                </ul>
                             </nav>
-                            </div>
+                        </div>
                         <!-- pagination -->
-
-
 
                     </div>
                 </div>
             </div>
             <!-- videos  -->
-
-
-          
-
-
-
-           
-
-      
-          
 
             <!-- footer includes -->
             <?php require_once("includes/footernav.php");?>
