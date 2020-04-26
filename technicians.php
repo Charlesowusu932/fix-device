a448973bcd00f0c3edbc5b77f7466f25b8d6b40a
Charles Owusu <kofidesign932@gmail.com>
2020-04-26 03:28:32 +0000

    Uodate Technicians

--- technicians.php
+++ technicians.php
@@ -71,16 +71,200 @@
             <!-- search side -->
 
 
-            <!-- contact info and map -->
+            <!-- Tecicians and advannce sort -->
               <section class="fixglobalspace2">
                <div class="container">
                    <div class="row">
+
+                   <div class="col-12 col-md-3">
+                     <div class="card">
+                         <div class="card-body">
+
+                         </div>
+                     </div>
+                   </div>
+
+                   <div class="col-12 col-md-9">
+                      <div class="row">
+
+                          <div class="col-12 text-right px-2 mb-4">
+                              <div class="card concard">
+                                  <div class="card-body">
+                                      <p class="mb-0">702 Result Found</p>
+                                  </div>
+                              </div>
+                          </div>
+
+                          <div class="col-12  col-md-4 mb-3 px-2 text-center">
+                              <a href="">
+                              <div class="card concard2">
+                                  <div class="card-body blacknormal2">
+                                  <img src="img/techArtboard2.jpg" class="rounded-circle tecni2 " alt="">
+                                      <h6 class="mb-0">  <strong>James Kphago</strong></h6>
+                                      <small>Computer Repair</small>
+                                      <hr>
+                                      <p class='mb-0 text-muted'> <span class="text-right">5 stars</span></p>
+
+                                  </div>
+                              </div>
+                              </a>
+                          </div>
+
+
+                          <div class="col-12  col-md-4 mb-3 px-2 text-center">
+                              <a href="">
+                              <div class="card concard2">
+                                  <div class="card-body blacknormal2">
+                                  <img src="img/techArtboard1.jpg" class="rounded-circle tecni2 " alt="">
+                                      <h6 class="mb-0">  <strong>Prosper Nii Amah</strong></h6>
+                                      <small>Phone Repair</small>
+                                      <hr>
+                                      <p class='mb-0 text-muted'> <span class="text-right">5 stars</span></p>
+
+                                  </div>
+                              </div>
+                              </a>
+                          </div>
+
+
+                          <div class="col-12  col-md-4 mb-3 px-2 text-center">
+                              <a href="">
+                              <div class="card concard2">
+                                  <div class="card-body blacknormal2">
+                                  <img src="img/techArtboard3.jpg" class="rounded-circle tecni2 " alt="">
+                                      <h6 class="mb-0">  <strong>Kate Odame</strong></h6>
+                                      <small>Cosole Repair</small>
+                                      <hr>
+                                      <p class='mb-0 text-muted'> <span class="text-right">5 stars</span></p>
+
+                                  </div>
+                              </div>
+                              </a>
+                          </div>
+
+                          <div class="col-12  col-md-4 mb-3 px-2 text-center">
+                              <a href="">
+                              <div class="card concard2">
+                                  <div class="card-body blacknormal2">
+                                  <img src="img/techArtboard1.jpg" class="rounded-circle tecni2 " alt="">
+                                      <h6 class="mb-0">  <strong>Prosper Nii Amah</strong></h6>
+                                      <small>Phone Repair</small>
+                                      <hr>
+                                      <p class='mb-0 text-muted'> <span class="text-right">5 stars</span></p>
+
+                                  </div>
+                              </div>
+                              </a>
+                          </div>
+
+                          <div class="col-12  col-md-4 mb-3 px-2 text-center">
+                              <a href="">
+                              <div class="card concard2">
+                                  <div class="card-body blacknormal2">
+                                  <img src="img/techArtboard2.jpg" class="rounded-circle tecni2 " alt="">
+                                      <h6 class="mb-0">  <strong>James Kphago</strong></h6>
+                                      <small>Computer Repair</small>
+                                      <hr>
+                                      <p class='mb-0 text-muted'> <span class="text-right">5 stars</span></p>
+
+                                  </div>
+                              </div>
+                              </a>
+                          </div>
+
+                          <div class="col-12  col-md-4 mb-3 px-2 text-center">
+                              <a href="">
+                              <div class="card concard2">
+                                  <div class="card-body blacknormal2">
+                                  <img src="img/techArtboard4.jpg" class="rounded-circle tecni2 " alt="">
+                                      <h6 class="mb-0">  <strong>Kofi Agyamang</strong></h6>
+                                      <small>Data Recovery</small>
+                                      <hr>
+                                      <p class='mb-0 text-muted'> <span class="text-right">5 stars</span></p>
+
+                                  </div>
+                              </div>
+                              </a>
+                          </div>
+
+                          
+                          <div class="col-12  col-md-4 mb-3 px-2 text-center">
+                              <a href="">
+                              <div class="card concard2">
+                                  <div class="card-body blacknormal2">
+                                  <img src="img/techArtboard3.jpg" class="rounded-circle tecni2 " alt="">
+                                      <h6 class="mb-0">  <strong>Kate Odame</strong></h6>
+                                      <small>Cosole Repair</small>
+                                      <hr>
+                                      <p class='mb-0 text-muted'> <span class="text-right">5 stars</span></p>
+
+                                  </div>
+                              </div>
+                              </a>
+                          </div>
+
+                          <div class="col-12  col-md-4 mb-3 px-2 text-center">
+                              <a href="">
+                              <div class="card concard2">
+                                  <div class="card-body blacknormal2">
+                                  <img src="img/techArtboard4.jpg" class="rounded-circle tecni2 " alt="">
+                                      <h6 class="mb-0">  <strong>Kofi Agyamang</strong></h6>
+                                      <small>Data Recovery</small>
+                                      <hr>
+                                      <p class='mb-0 text-muted'> <span class="text-right">5 stars</span></p>
+
+                                  </div>
+                              </div>
+                              </a>
+                          </div>
+
+
+                          <div class="col-12  col-md-4 mb-3 px-2 text-center">
+                              <a href="">
+                              <div class="card concard2">
+                                  <div class="card-body blacknormal2">
+                                  <img src="img/techArtboard2.jpg" class="rounded-circle tecni2 " alt="">
+                                      <h6 class="mb-0">  <strong>James Kphago</strong></h6>
+                                      <small>Computer Repair</small>
+                                      <hr>
+                                      <p class='mb-0 text-muted'> <span class="text-right">5 stars</span></p>
+
+                                  </div>
+                              </div>
+                              </a>
+                          </div>
+
+
+                          <!-- pagination -->
+                            <div class="col-12 mt-4">
+                            <nav aria-label="Page navigation example">
+                            <ul class="pagination justify-content-center">
+                                <li class="page-item disabled">
+                                <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+                                </li>
+                                <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                <li class="page-item">
+                                <a class="page-link" href="#">Next</a>
+                                </li>
+                            </ul>
+                            </nav>
+                            </div>
+                          <!-- pagination -->
+
+
+
+                          
+
+                      </div>
+                   </div>
                        
                        
                </div>
 
-              </section>
-             <!-- contact info and map -->
+              </section> 
+            <!-- Tecicians and advannce sort -->
 
            
 
