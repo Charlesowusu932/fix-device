be0945813515fc04ad8cbb63e97037c8364233c8
Charles Owusu <kofidesign932@gmail.com>
2020-04-29 14:08:52 +0000

    Add Fix Device Page

---
+++ fix.php
@@ -0,0 +1,121 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Fix Devices | Fix Device</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+
+    <!-- include navigation -->
+    <?php require_once("includes/headerlinks.php");?>
+        <?php require_once("includes/navigation.php");?>
+            <!-- include navigation -->
+
+            <!-- top cover -->
+            <section class="technician centerit">
+                <div class="container ">
+                    <div class="row ">
+
+                        <div class="col-12 text-center mt-5 ">
+                            <h1 class="bighead">Fix Design</h1>
+                            <p class="white">Get your device fixed, Select your option</p>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- top cover -->
+
+            <!-- fix device select option -->
+            <section class="faqcup">
+                <div class="container">
+                    <div class="row">
+
+                        <div class="col-12 col-md-8 mx-auto">
+                            <div class="row">
+
+                                <div class="col-6 col-md-4 text-center mb-3">
+                                    <a href="">
+                                    <div class="card faqcard shadow">
+                                        <div class="card-body">
+                                            <img src="img/faqflag.svg" alt="">
+                                            <p class="faqtoptabtt balcksmall mt-2">Getting Started</p>
+                                        </div>
+                                    </div>
+                                    </a>
+                                </div>
+
+                                <div class="col-6 col-md-4 text-center mb-3">
+                                    <a href="">
+                                    <div class="card faqcard shadow">
+                                        <div class="card-body">
+                                            <img src="img/faqpricing.svg" alt="">
+                                            <p class="faqtoptabtt balcksmall mt-2">Pricing</p>
+                                        </div>
+                                    </div>
+                                    </a>
+                                </div>
+
+                                <div class="col-6 col-md-4 text-center mb-3">
+                                    <a href="">
+                                    <div class="card faqcard shadow">
+                                        <div class="card-body">
+                                            <img src="img/faqguid.svg" alt="">
+                                            <p class="faqtoptabtt balcksmall mt-2">User Guide</p>
+                                        </div>
+                                    </div>
+                                    </a>
+                                </div>
+
+                                <div class="col-6 col-md-4 text-center mb-3">
+                                    <a href="">
+                                    <div class="card faqcard shadow">
+                                        <div class="card-body">
+                                            <img src="img/faqInformation.svg" alt="">
+                                            <p class="faqtoptabtt balcksmall mt-2">Information</p>
+                                        </div>
+                                    </div>
+                                    </a>
+                                </div>
+
+                                <div class="col-6 col-md-4 text-center mb-3">
+                                    <a href="">
+                                    <div class="card faqcard shadow">
+                                        <div class="card-body">
+                                            <img src="img/faqInformation.svg" alt="">
+                                            <p class="faqtoptabtt balcksmall mt-2">Information</p>
+                                        </div>
+                                    </div>
+                                    </a>
+                                </div>
+
+                                <div class="col-6 col-md-4 text-center mb-3">
+                                    <a href="">
+                                    <div class="card faqcard shadow">
+                                        <div class="card-body">
+                                            <img src="img/faqInformation.svg" alt="">
+                                            <p class="faqtoptabtt balcksmall mt-2">Information</p>
+                                        </div>
+                                    </div>
+                                    </a>
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+            <!-- fix device select optio -->
+
+            <!-- footer includes -->
+            <?php require_once("includes/footernav.php");?>
+                <?php require_once("includes/footerlinks.php");?>
+                    <!-- footer includes -->
+
+                    </body>
+
+</html>
